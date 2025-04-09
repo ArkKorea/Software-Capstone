1fdf029dcca07fde4370ee1b1d93190dd86e91b4
ArkKorea <cjjj1102@naver.com>
2025-04-10 00:49:10 +0900

    user.gender , favorites.bundle_id, supplier_id, type 추가

--- schema/Utils.sql
+++ schema/Utils.sql
@@ -1,46 +1,5 @@
 use test;
 
-/*
--- 인당 기록 최근 30개 유지 프로시저
-
-DELIMETER //
-
-CREATE PROCEDURE manage_view_log(IN p_user_id INT)
-BEGIN
-    DECLARE row_count INT;
-
-    SELECT COUNT(*) INTO row_count
-    FROM view_log
-    WHERE user_id = p_user_id;
-
-    WHILE row_count > 30 DO
-        DELETE v FROM view_log v
-        JOIN (
-            SELECT id FROM view_log
-            WHERE user_id = p_user_id
-            ORDER BY viewed_at ASC
-            LIMIT 1
-        ) AS sub ON v.id = sub.id;
-
-        SET row_count = row_count -1;
-    END WHILE;
-END //
-
-DELIMETER ;
-
--- 위 프로시저 실행을 위한 트리거 작성
-DELIMETER //
-
-CREATE TRIGGER after_insert_view_log
-AFTER INSERT ON view_log
-FOR EACH ROW
-BEGIN
-    CALL manage_view_log(NEW.user_id);
-END //
-
-DELIMETER ;
-*/
-
 DELIMITER //
 
 CREATE TRIGGER after_insert_view_log
