723da788a4bef78a4ce94bb284da37cff7ee619f
LemonSoda7320 <113896738+LemonSoda7320@users.noreply.github.com>
2025-05-07 23:22:47 +0900

    Update main.py
    충돌 해결

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.api import auth, product, search
 
+
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
 app.include_router(product.router, prefix="/api/product", tags=["Product"])
+app.include_router(favorite.router, prefix="/api/user", tags=["Favorites"])
 app.include_router(search.router, prefix="/api", tags=["Search"])
