bf467ba3455eeaab15405de3cc4b392c081a2c13
ArkKorea <cjjj1102@naver.com>
2025-05-10 01:23:16 +0900

    import 수정

--- app/api/allergen.py
+++ app/api/allergen.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.schemas.allergen import AllergenGetResponse, AllergenSaveRequest, AllergenSaveResponse
+from app.services.allergen_service import get_user_allergies, save_user_allergies
 router = APIRouter()
 
-@router.post("/get")
-def get_allergies(request, db: Session = Depends(get_db)):
-    return
+@router.post("/get", response_model=AllergenGetResponse)
+def get_allergies(db: Session = Depends(get_db)):
+    return get_user_allergies(db)
 
 
 @router.post("/save", response_model=AllergenSaveResponse)
 def save_allergies(request: AllergenSaveRequest, db: Session = Depends(get_db)):
-    return AllergenSaveResponse()
+    return save_user_allergies(request, db)
