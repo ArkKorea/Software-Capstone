e4d809fe9bf15b03c3a9c3cdf758e2f0a81ab223
ArkKorea <cjjj1102@naver.com>
2025-05-09 16:39:51 +0900

    번들 수정

--- app/models/food_bundle.py
+++ app/models/food_bundle.py
@@ -7,7 +7,7 @@ from typing import List
 from app.models.food_bundle_item import FoodBundleItem
 from app.models.supplier import Supplier
 from app.models.qr_link import QrLink
-
+from .food import Food
 class FoodBundle(Base):
     __tablename__ = "food_bundles"
 
@@ -18,5 +18,5 @@ class FoodBundle(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     supplier: Mapped['Supplier'] = relationship("Supplier", back_populates="bundles")
-    items: Mapped[List['FoodBundleItem']] = relationship("FoodBundleItem", back_populates="bundle")
+    items: Mapped[List['Food']] = relationship("Food", back_populates="bundles")
     qr_link: Mapped['QrLink'] = relationship('QrLink', back_populates='bundle')
