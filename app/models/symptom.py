4bbe01ebfc3b7cc9fe261fbdf4ca0ceceafb845c
lemonsoda <xerophyte7320@gmail.com>
2025-05-21 01:59:16 +0900

    SymptomsLog의 오류 수정

--- app/models/symptom.py
+++ app/models/symptom.py
@@ -9,11 +9,11 @@ class SymptomsLog(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     user_id: Mapped[Optional[int]] = mapped_column(Integer)
-    skin: Mapped[int] = mapped_column(Integer),
-    stomach: Mapped[int] = mapped_column(Integer),
-    breath: Mapped[int] = mapped_column(Integer),
-    headache: Mapped[int] = mapped_column(Integer),
-    fatigue: Mapped[int] = mapped_column(Integer),
+    skin: Mapped[int] = mapped_column(Integer)
+    stomach: Mapped[int] = mapped_column(Integer)
+    breath: Mapped[int] = mapped_column(Integer)
+    headache: Mapped[int] = mapped_column(Integer)
+    fatigue: Mapped[int] = mapped_column(Integer)
     log_time: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     user: Mapped[Optional['Users']] = relationship('Users', back_populates='symptoms_logs')
