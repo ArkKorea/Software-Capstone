b53ed9a98cd8de88d5c1387f2fa4794de74655e5
lemonsoda <xerophyte7320@gmail.com>
2025-06-10 01:55:29 +0900

    수정

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "super-secret-key" # 실제는 환경변수에서 불러오게 설정해야 함. 테스트 용도로 일단 했음
+    SECRET_KEY: str = "" # 실제는 환경변수에서 불러오게 설정해야 함. 테스트 용도로 일단 했음
     DATABASE_URL: str
 
     class Config:
